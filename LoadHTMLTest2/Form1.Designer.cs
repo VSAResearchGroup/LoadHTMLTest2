c5ab02e05472f25e7e298e56947e610a1f726555
tunglequang <tunglq89@uw.edu>
2017-11-21 15:44:34 -0800

    The HTML stripping tool

---
+++ LoadHTMLTest2/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace LoadHTMLTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCrawl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtResult.Location = new System.Drawing.Point(12, 12);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(537, 359);
+            this.txtResult.TabIndex = 0;
+            // 
+            // btnCrawl
+            // 
+            this.btnCrawl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCrawl.Location = new System.Drawing.Point(12, 377);
+            this.btnCrawl.Name = "btnCrawl";
+            this.btnCrawl.Size = new System.Drawing.Size(75, 23);
+            this.btnCrawl.TabIndex = 1;
+            this.btnCrawl.Text = "Crawl";
+            this.btnCrawl.UseVisualStyleBackColor = true;
+            this.btnCrawl.Click += new System.EventHandler(this.btnCrawl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 409);
+            this.Controls.Add(this.btnCrawl);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCrawl;
+    }
+}
+
---
+++ LoadHTMLTest2/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using HtmlAgilityPack;
+using System.Xml;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace LoadHTMLTest2
+{
+    public partial class Form1 : Form
+    {
+        SqlCommand cmd;
+        SqlConnection con;
+        //SqlDataAdapter da;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrawl_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=uwstrikersserver.database.windows.net;Initial Catalog=PriceStrikers;Persist Security Info=True;User ID=imdaboss;Password=Youdaboss1");
+            //con.Open();
+            String planTitle="";
+            //String courseName = "MATH 142";
+            String quarterPlanTitle="Fall2019";
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            doc = web.Load("C:/Users/tungl/Desktop/DegreeAudit_12.html");
+            HtmlNodeCollection planTitleNode = doc.DocumentNode.SelectNodes("//*[@id='DD_PlanTitle']");
+            foreach (var item in planTitleNode)
+            {
+                planTitle = item.InnerText;
+                MessageBox.Show(planTitle);
+            }
+            for(int i = 1; i <= 20; i++)
+            {
+                String xpath = "//*[@id='QP" + i + "_LBL_QPlanTitle']";
+                HtmlNodeCollection quarterPlanTitleNode = doc.DocumentNode.SelectNodes(xpath);
+                foreach (var item in quarterPlanTitleNode)
+                {
+                    quarterPlanTitle = item.InnerText;
+                    MessageBox.Show(quarterPlanTitle);
+                }
+                for (int j = 1; j <= 5; j++)
+                {
+                    String xpath2 = "//*[@id='QP"+i+"_TB_Line" + j + "']";
+                    String val = doc.DocumentNode.SelectSingleNode(xpath2).GetAttributeValue("value", "default");
+                    if (val != "") 
+                        MessageBox.Show(val);
+
+                }
+            }
+
+            /*string query = "insert into studyPlan select planID, courseID  from PLANS, course where planName = '"+planTitle+"' AND courseName = '"+courseName+"'; ";
+            cmd = new SqlCommand(query, con);
+            SqlDataReader myreader;
+            try
+            {
+                con.Open();
+                myreader = cmd.ExecuteReader();
+                MessageBox.Show("Saved");
+                while (myreader.Read())
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }*/
+
+            /*string query = "INSERT INTO AcademicPlan VALUES('" + studentName + "','" + studentID + "','" + planTitle + "','" + quarterPlanTitle + "');";
+            cmd = new SqlCommand(query, con);
+            SqlDataReader myreader;
+            try
+            {
+                con.Open();
+                myreader = cmd.ExecuteReader();
+                MessageBox.Show("Saved");
+                while (myreader.Read())
+                {
+
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }*/
+             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
